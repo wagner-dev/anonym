d3a357cf278a21f0bf92c72f7be3a6f408c54e37
wagner-dev <wagnerl.9444@gmail.com>
2021-10-01 13:52:11 -0300

    ordenar usuários por seguidores(decrescente) - page search

--- client/src/pages/client/search/index.jsx
+++ client/src/pages/client/search/index.jsx
@@ -4,7 +4,6 @@ import {
 } from './styled/index'
 import Search from './components/search/index'
 import api from '../../../services/api/index'
-import { getPersist } from '../../../services/persist/index' 
 
 export default function SearchComponent(){
 
@@ -14,8 +13,7 @@ export default function SearchComponent(){
     const [load, setLoad] = useState({initial: true, load: false})
 
     async function RequestSearch(value){
-        const token = getPersist()
-        const { data, status } = await api.get(`/user/search?q=${value}`, {headers: {Authorization: `Bearer ${token}`}})
+        const { data, status } = await api.get(`/user/search?q=${value}`)
         if(data.status === 200 && status === 200){
             // desabilitar load
             setLoad({load: false})
--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -326,22 +326,18 @@ module.exports = {
 
     },
     async search(req, res) {
-        const token = req.headers.authorization.split(" ")[1]
         const quest = req.query.q
         try{
-            if(quest && token){
-                const validToken = verifyToken(token)
-
-                if(validToken){
-                    const search = await User.find({username: {$regex: quest}}, {_id: 0,img_profile: 1, username: 1, desc: 1, is_verified: 1})
-                    res.json({message: 'ok', result: search, status: 200})
-                }
-                else{
-                    res.json({message: 'Não autorizado', status: 401})
-                }
+            if(quest){
+                const search = await User.find({username: {$regex: quest}}, {_id: 0,img_profile: 1, username: 1, desc: 1, is_verified: 1, followers: 1})
+                // ordenar
+                search.sort((a, b) => b.followers.length - a.followers.length)
+                // retirar ids dos users
+                search.map(e => e.followers = undefined)
+                res.json({message: 'ok', result: search, status: 200})
             }
             else{
-                res.json({message: 'Não autorizado', status: 401})
+                res.json({message: 'Busca inválida', status: 401})
             }
         }
         catch{
