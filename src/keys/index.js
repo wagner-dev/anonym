35b0775c01bd6222c42619baa288c46b915e6334
wagner-dev <wagnerl.9444@gmail.com>
2021-10-04 17:26:00 -0300

    Configurando variáveis de ambiente

--- src/keys/index.js
+++
@@ -1,5 +0,0 @@
-const keys = {
-    "jwt": ")D(:T7ex(QrF@PcUoZ#$t-8!2>fn[hg!kP95,ES6U@@:[FShf<L?*-1j*<,8fUk"
-}
-
-module.exports = keys
--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const keys = require('../../keys/index')
 module.exports = {
     verifyToken( token ){
+        const SECRET = process.env.SECRET_JWT
 
-        if(token) return jwt.verify(token, keys.jwt ,(err, result) => err ? false : result)
+        if(token) return jwt.verify(token, SECRET ,(err, result) => err ? false : result)
         else return false
 
     }
